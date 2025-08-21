b513ae80fa64a2fcf339e0007241da5dab2f5b0d
lingu <lingu@paynstore.co.kr>
2025-08-21 21:34:49 +0900

    불필요한 파일제거

--- app/src/main/java/helium314/keyboard/latin/App.kt
+++ app/src/main/java/helium314/keyboard/latin/App.kt
@@ -6,7 +6,6 @@ import android.content.Context
 import androidx.core.content.edit
 import helium314.keyboard.keyboard.ColorSetting
 import helium314.keyboard.keyboard.KeyboardTheme
-import helium314.keyboard.keyboard.emoji.SupportedEmojis
 import helium314.keyboard.keyboard.internal.keyboard_parser.floris.KeyCode.checkAndConvertCode
 import helium314.keyboard.latin.common.ColorType
 import helium314.keyboard.latin.common.Constants.Separators
@@ -54,7 +53,6 @@ class App : Application() {
         app = this
         Defaults.initDynamicDefaults(this)
         LayoutUtilsCustom.removeMissingLayouts(this) // only after version upgrade
-        SupportedEmojis.load(this)
 
         val packageInfo = packageManager.getPackageInfo(packageName, 0)
         @Suppress("DEPRECATION")
